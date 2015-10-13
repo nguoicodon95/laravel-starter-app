afb6df0c85c2137aa0cc55c0f28ce120c594dff1
James Star <jamess.developer@gmail.com>
2015-10-13 20:56:21 +1300

    chore: add share buttons

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -26,6 +26,18 @@
     </style>
   </head>
   <body ng-app="streamApp">
+
+    <!-- Facebook SDK -->
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+    <!-- End of Facebook SDK -->
+
     <nav class="navbar navbar-inverse">
       <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
