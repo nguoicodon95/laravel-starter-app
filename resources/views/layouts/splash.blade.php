8174b47f4c20d636fe3791b39db0245a9a52170b
jamesstar <james.star@touchcast.co.nz>
2016-01-09 14:26:50 +1300

    Patch: v2.0.1 meta viewport

--- resources/views/layouts/splash.blade.php
+++ resources/views/layouts/splash.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <title>Puff Stream</title>
     <link href="/assets/css/libs.css" rel="stylesheet" />
     <link href="/assets/css/app.css" rel="stylesheet" />
