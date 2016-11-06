9448d4fdf5f1d1d18a5d110d892271202d1aa97e
James Star <jamess.developer@gmail.com>
2016-11-06 13:31:15 +1300

    Add new migrations for initial business logic

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,5 +12,5 @@
 */
 
 Route::get('/', function () {
-    return view('book-example');
+    return view('app');
 });
---
+++ resources/views/app.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Stream</title>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="/css/app.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-6">
+                    <h1>New message</h1>
+                    <select class="form-control">
+                        <option>Select</option>
+                        <option>Color</option>
+                        <option>Post</option>
+                    </select>
+                    <h1>Color</h1>
+                    <input type="text" class="form-control" placeholder="Color name" />
+                    <br />
+                    <input type="text" class="form-control" placeholder="#" />
+                    <br />
+                    <input type="button" class="btn btn-primary" value="Publish" />
+                    <h1>Post</h1>
+                    <textarea class="form-control"></textarea>
+                    <br />
+                    <input type="button" class="btn btn-primary" value="Publish" />
+                </div>
+            </div>
+        </div>
+        <script src="/js/app.js"></script>
+    </body>
+</html>
